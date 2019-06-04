8b9887aea32c04e7b241231d74011c218edf4a57
Perfeito <fabersonperfeito@hotmail.com>
2019-06-04 01:25:02 -0300

    Estrutura do projeto Xamarin

---
+++ src/AtividadesVeiculoColeta/AtividadesVeiculoColeta/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using AtividadesVeiculoColeta.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AtividadesVeiculoColeta
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
