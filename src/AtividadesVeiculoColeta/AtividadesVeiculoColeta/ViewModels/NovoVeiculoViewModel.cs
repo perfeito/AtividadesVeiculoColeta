9dd3913fdcd29bb01afb1c6ffa0377479de3f738
Perfeito <fabersonperfeito@hotmail.com>
2019-06-04 01:28:45 -0300

    VeiculoViewModel adicionado, contem campos basicos como placa e nome

---
+++ src/AtividadesVeiculoColeta/AtividadesVeiculoColeta/ViewModels/NovoVeiculoViewModel.cs
@@ -0,0 +1,43 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace AtividadesVeiculoColeta.ViewModels
+{
+    public class NovoVeiculoViewModel : ObservableObject
+    {
+        public NovoVeiculoViewModel()
+        {
+
+        }
+
+        private string _nome;
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+            set
+            {
+                SetProperty(ref _nome, value);
+            }
+
+        }
+
+        private string _placa;
+        public string Placa
+        {
+            get
+            {
+                return _placa;
+            }
+            set
+            {
+                SetProperty(ref _placa, value);
+            }
+        }
+    }
+}
